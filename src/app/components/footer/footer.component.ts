bf2e244236c00d2e4dd0378114b39adfcd3b8de5
Mouhamadou-Ndiaye <mouhamadoundiaye1290@gmail.com>
2025-02-08 15:10:13 +0100

    header and footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';  // Importer le module mat-toolbar
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-footer',
   standalone: true,  // Si c'est un composant standalone
-  imports: [MatToolbarModule],  // Assurez-vous que MatToolbarModule est importé
+  imports: [MatToolbarModule, MatIconModule],  // Assurez-vous que MatToolbarModule est importé
   styleUrls: ['./footer.component.scss'],
   templateUrl: './footer.component.html',
 })
-export class FooterComponent {}
+export class FooterComponent {
+  currentYear: number = new Date().getFullYear();
+}
